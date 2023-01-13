591252b369491d1cbfbef6b39fabdf8a271e170b
tada5hi <peter.placzek1996@gmail.com>
2023-01-13 20:14:00 +0100

    fix(core): updated mime dependency

--- packages/core/src/utils/mime.ts
+++ packages/core/src/utils/mime.ts
@@ -5,7 +5,7 @@
  * view the LICENSE file that was distributed with this source code.
  */
 
-import { get, getType } from 'mime-es';
+import { get, getType } from 'mime-explorer';
 
 export function getMimeType(type: string) : string | undefined {
     if (type.indexOf('/') !== -1) {
