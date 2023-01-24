aeabf06d2372f315bdbe33546ea5dacb74ce6d9d
tada5hi <peter.placzek1996@gmail.com>
2023-01-24 18:43:45 +0100

    feat: use rollup + swc for transpiling and bundle code for esm/cjs

---
+++ rollup.config.mjs
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2022.
+ * Author Peter Placzek (tada5hi)
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
+import { builtinModules } from 'module';
+import { transform } from "@swc/core";
+
+const extensions = [
+    '.js', '.mjs', '.ts',
+];
+
+const swcOptions = {
+    jsc: {
+        target: 'es2020',
+        parser: {
+            syntax: 'typescript',
+            decorators: true
+        },
+        transform: {
+            decoratorMetadata: true,
+            legacyDecorator: true
+        },
+        loose: true
+    },
+    sourceMaps: true
+}
+
+export function createConfig({ pkg, external = [] }) {
+    external = Object.keys(pkg.dependencies || {})
+        .concat(Object.keys(pkg.peerDependencies || {}))
+        .concat(builtinModules)
+        .concat(external);
+
+    return {
+        input: 'src/index.ts',
+        external,
+        output: [
+            {
+                format: 'cjs',
+                file: pkg.main,
+                exports: 'named',
+                sourcemap: true
+            },
+            {
+                format: 'es',
+                file: pkg.module,
+                exports: 'named',
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            // Allows node_modules resolution
+            resolve({ extensions}),
+
+            // Allow bundling cjs modules. Rollup doesn't understand cjs
+            commonjs(),
+
+            // Compile TypeScript/JavaScript files
+            {
+                name: 'swc',
+                transform(code) {
+                    return transform(code, swcOptions);
+                }
+            },
+        ]
+    };
+}
