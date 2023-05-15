9dbd2ff7792f57522146d0e90f506b21fb73f882
tada5hi <peter.placzek1996@gmail.com>
2023-05-15 14:37:45 +0200

    build: remove copyright in test and build files

--- test/unit/routing/router.spec.ts
+++ test/unit/routing/router.spec.ts
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2022-2022.
- * Author Peter Placzek (tada5hi)
- * For the full copyright and license information,
- * view the LICENSE file that was distributed with this source code.
- */
-
 import { GatewayTimeoutErrorOptions, NotFoundErrorOptions } from '@ebec/http';
 import supertest from 'supertest';
 import { Router, send, useRequestParams } from '../../../src';
