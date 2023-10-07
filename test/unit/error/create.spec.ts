e8495f0b9494117dcb1fc3700eb129802b16ea05
tada5hi <peter.placzek1996@gmail.com>
2023-10-07 10:27:28 +0200

    fix(deps): bump @ebec/http to v2.3.0 & check for inherited properties

--- test/unit/error/create.spec.ts
+++ test/unit/error/create.spec.ts
@@ -1,3 +1,4 @@
+import { InternalServerError, NotFoundError } from '@ebec/http';
 import { ErrorProxy, createError } from '../../../src';
 
 describe('src/error/create', () => {
@@ -16,5 +17,25 @@ describe('src/error/create', () => {
         const error = createError({ statusCode: 510, statusMessage: 'foo' });
         expect(error.statusCode).toEqual(510);
         expect(error.statusMessage).toEqual('foo');
+    });
+
+    it('should create error for client error', () => {
+        const notFoundError = new NotFoundError();
+        const error = createError(notFoundError);
+
+        expect(error).toBeDefined();
+        expect(error.statusCode).toEqual(notFoundError.statusCode);
+        expect(error.statusMessage).toEqual(notFoundError.statusMessage);
+        expect(error.expose).toBeTruthy();
+    });
+
+    it('should create error for server error', () => {
+        const internalServerError = new InternalServerError();
+        const error = createError(internalServerError);
+
+        expect(error).toBeDefined();
+        expect(error.statusCode).toEqual(internalServerError.statusCode);
+        expect(error.statusMessage).toEqual(internalServerError.statusMessage);
+        expect(error.expose).toBeFalsy();
     });
 });
